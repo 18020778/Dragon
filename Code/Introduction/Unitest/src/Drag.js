dd0cb3d2be963208166c91fcdfe1724753cd5de7
TrinhNt14 <56534143+TrinhNt14@users.noreply.github.com>
2020-05-04 13:43:06 +0700

    #16
    Kiểm thử kéo thả nhân vật

---
+++ Code/Introduction/Unitest/src/Drag.js
@@ -0,0 +1,22 @@
+class dragObject{
+    constructor(left, top, height){
+        this.left = left;
+        this.top = top;
+        this.height = height;
+        this.dragTop = top + height;
+    }
+    dropObject(left, top){
+        this.limitTopMin = top - 5;
+        this.limitTopMax = top + 50;
+        this.limitLeftMin = left;
+        this.limitLeftMax = left + 120;
+    }
+    checkDrag(){
+        if( this.dragTop >= this.limitTopMin && this.dragTop <= this.limitTopMax){
+            if(this.left >= this.limitLeftMin && this.left <= this.limitLeftMax){
+                return true;
+            }
+        }
+        return false;
+    }
+}
