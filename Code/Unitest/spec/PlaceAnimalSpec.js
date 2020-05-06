fd25d897ba8b1f7116b080bd2d59ead833a2a337
missaut1309 <57839645+missaut1309@users.noreply.github.com>
2020-05-06 15:17:37 +0700

    Update Unit Test

--- Code/Unitest/spec/PlaceAnimalSpec.js
+++ Code/Unitest/spec/PlaceAnimalSpec.js
@@ -1,39 +1,44 @@
+// Lưu ý: phải tắt chế độ "run test in random order"
+// do các di chuyển nhân vật phía sau có liên quan đến di chuyển nhân vật phía trước. 
+
 describe("Bài 2 màn 1", function() {
 const y_land = 100;
-const bear = new Character(5,100);
-const wolf = new Character(-1,-1);
-const fox = new Character(305,100);
-const raccoon = new Character(-1,-1);
-const rabbit = new Character(605,100);
-const owl = new Character(-1,-1);
+const bear = new Character(5,y_land);
+var wolf = new Character(-1,-1);
+const fox = new Character(305,y_land);
+var raccoon = new Character(-1,-1);
+const rabbit = new Character(605,y_land);
+var owl = new Character(-1,-1);
+var orderArray = [1,0,1,0,1,0];
+
 describe("Vị trí ban đầu của nhân vật", function() {
   it("Con gấu ở vị trí thứ nhất",function() {
-    expect(bear.order()).toBe(1);
+    expect(bear.order()).toBe(0);
     expect(isInLine(bear, y_land)).toEqual(true);
   });
   
   it("Con thỏ ở vị trí thứ năm", function() {
-    expect(rabbit.order()).toBe(5);
+    expect(rabbit.order()).toBe(4);
     expect(isInLine(rabbit, y_land)).toEqual(true);
   });
   
   it("Con cáo ở vị trí thứ ba", function() {
-    expect(fox.order()).toBe(3);
+    expect(fox.order()).toBe(2);
     expect(isInLine(fox, y_land)).toEqual(true);
   });
   
 });
 describe("Di chuyển nhân vật đến vị trí yêu cầu", function(){
   it("Di chuyển con sói đến cạnh con gấu", function(){
-    moveToOrder(wolf, bear.order() + 1, y_land);
+    moveToBeside(wolf,bear,orderArray);
     expect(isBeside(bear,wolf)).toEqual(true);
   });
   it("Di chuyển con gấu mèo vào giữa con cáo và con thỏ", function(){
-    moveToOrder(raccoon, fox.order() + 1, y_land);
-    expect(isBetween(fox, rabbit, raccoon)).toEqual(true);
+    moveToBetween(raccoon,fox,rabbit,orderArray);
+    expect(isBetween(fox,rabbit,raccoon)).toEqual(true);
   });
   it("Di chuyển con cú đến bên cạnh con thỏ", function(){
-    moveToOrder(owl,6,y_land);
+    moveToBeside(owl,rabbit,orderArray);
     expect(isBeside(rabbit, owl)).toEqual(true);
   });
 });
@@ -42,93 +47,79 @@ describe("Di chuyển nhân vật đến vị trí yêu cầu", function(){
 
 describe("Bài 2 màn 2", function() {
   const y_land = 100;
-  const pear = new Character(-1,-1);
-  const banana = new Character(150,100);
-  const grapes = new Character(-1,-1);
-  const orange = new Character(-1,-1);
-  const apple = new Character(605,100);
-  const watermelon = new Character(-1,-1);
+  var pear = new Character(-1,-1);
+  var banana = new Character(150,100);
+  var grapes = new Character(-1,-1);
+  var orange = new Character(-1,-1);
+  var apple = new Character(605,100);
+  var watermelon = new Character(-1,-1);
+  var orderArray = [0, 1, 0, 0, 1, 0];
   describe("Vị trí ban đầu của nhân vật", function() {
     it("Quả chuối ở vị trí thứ hai",function() {
-      expect(banana.order()).toBe(2);
+      expect(banana.order()).toBe(1);
       expect(isInLine(banana, y_land)).toEqual(true);
     });
     it("Quả táo ở vị trí thứ năm", function() {
-      expect(apple.order()).toBe(5);
+      expect(apple.order()).toBe(4);
       expect(isInLine(apple, y_land)).toEqual(true);
     });
   });
   describe("Di chuyển nhân vật đến vị trí yêu cầu", function(){
     it("Di chuyển quả cam đến cạnh quả táo", function(){
-      moveToOrder(orange, apple.order() + 1, y_land);
-      expect(isBeside(orange,apple)).toEqual(true);
-    });
-    it("Di chuyển quả cam đến cạnh quả táo", function(){
-      moveToOrder(orange, apple.order() - 1, y_land);
+      moveToBeside(orange,apple,orderArray);
       expect(isBeside(orange,apple)).toEqual(true);
     });
     it("Di chuyển chùm nho đến bên cạnh quả chuối", function(){
-      moveToOrder(grapes, banana.order() + 1, y_land);
-      expect(isBeside(grapes,banana)).toEqual(true);
-    });
-    it("Di chuyển chùm nho đến bên cạnh quả chuối", function(){
-      moveToOrder(grapes, banana.order() - 1, y_land);
+      moveToBeside(grapes,banana,orderArray);
       expect(isBeside(grapes,banana)).toEqual(true);
     });
     it("Di chuyển quả lê đến bên cạnh quả chuối", function(){
-      moveToOrder(pear,banana.order() - 1,y_land);
+      moveToBeside(pear,banana,orderArray);
       expect(isBeside(pear, banana)).toEqual(true);
     });
-    it("Di chuyển quả lê đến bên cạnh quả chuối", function(){
-      moveToOrder(pear,banana.order() + 1,y_land);
-      expect(isBeside(pear, banana)).toEqual(true);
-    });
-    it("Di chuyển miếng dư hấu đến bên cạnh quả táo", function(){
-      moveToOrder(watermelon,apple.order() - 1,y_land);
+    it("Di chuyển miếng dưa hấu đến bên cạnh quả táo", function(){
+      moveToBeside(watermelon,apple,orderArray);
       expect(isBeside(watermelon, apple)).toEqual(true);
     });
-    it("Di chuyển miếng dư hấu đến bên cạnh quả táo", function(){
-      moveToOrder(watermelon,apple.order() + 1,y_land);
-      expect(isBeside(watermelon, apple)).toEqual(true);
-    });
+
   });
 });
 
 
 describe("Bài 2 màn 3", function() {
   const y_land = 100;
-  const cupcake = new Character(5,100);
-  const cup = new Character(-1,-1);
-  const icecream = new Character(-1,-1);
-  const teapot = new Character(455,100);
-  const juice = new Character(-1,-1);
-  const dessert = new Character(755,100);
+  var cupcake = new Character(5,y_land);
+  var cup = new Character(-1,-1);
+  var icecream = new Character(-1,-1);
+  var teapot = new Character(455,y_land);
+  var juice = new Character(-1,-1);
+  var dessert = new Character(755,y_land);
+  var orderArray = [1, 0, 0, 1, 0, 1];
   describe("Vị trí ban đầu của nhân vật", function() {
     it("Bánh ở vị trí thứ nhất",function() {
-      expect(cupcake.order()).toBe(1);
+      expect(cupcake.order()).toBe(0);
       expect(isInLine(cupcake, y_land)).toEqual(true);
     });
     it("Bình trà ở vị trí thứ tư", function() {
-      expect(teapot.order()).toBe(4);
+      expect(teapot.order()).toBe(3);
       expect(isInLine(teapot, y_land)).toEqual(true);
     });
     it("Món tráng miệng ở vị trí thứ sáu", function() {
-      expect(dessert.order()).toBe(6);
+      expect(dessert.order()).toBe(5);
       expect(isInLine(dessert, y_land)).toEqual(true);
     });
   });
   describe("Di chuyển nhân vật đến vị trí yêu cầu", function(){
     it("Di chuyển ly trà đến cạnh bánh", function(){
-      moveToOrder(cup, cupcake.order() + 1, y_land);
+      moveToBeside(cup,cupcake,orderArray);
       expect(isBeside(cup,cupcake)).toEqual(true);
     });
     it("Di chuyển cây kem vào giữa ly trà và bình trà", function(){
-      moveToOrder(cup, cupcake.order() + 1, y_land);
-      moveToOrder(icecream, cup.order() + 1, y_land);
+      moveToBetween(icecream,cup,teapot,orderArray);
       expect(isBetween(cup, teapot, icecream)).toEqual(true);
     });
     it("Di chuyển nước ép vào giữa bình trà và món tráng miệng", function(){
-      moveToOrder(juice,teapot.order() + 1,y_land);
+      moveToBetween(juice,teapot,dessert,orderArray);
       expect(isBetween(teapot, dessert, juice)).toEqual(true);
     });
   });
@@ -137,41 +128,41 @@ describe("Bài 2 màn 3", function() {
 
 describe("Bài 2 màn 4", function() {
   const y_land = 100;
-  const wolf = new Character(5,100);
-  const raccoon = new Character(-1,-1);
-  const fox = new Character(-1,-1);
-  const rabbit = new Character(-1,-1);
-  const bear = new Character(605,100);
-  const owl = new Character(-1,-1);
+  var wolf = new Character(5,y_land);
+  var raccoon = new Character(-1,-1);
+  var fox = new Character(-1,-1);
+  var rabbit = new Character(-1,-1);
+  var bear = new Character(-1,-1);
+  var owl = new Character(755,y_land);
+  var orderArray = [1, 0, 0, 0, 0, 1];
   describe("Vị trí ban đầu của nhân vật", function() {
     it("Con sói ở vị trí thứ nhất",function() {
-      expect(wolf.order()).toBe(1);
+      expect(wolf.order()).toBe(0);
       expect(isInLine(wolf, y_land)).toEqual(true);
     });
-    it("Con gấu ở vị trí thứ năm", function() {
-      expect(bear.order()).toBe(5);
-      expect(isInLine(bear, y_land)).toEqual(true);
+    it("Con cú ở vị trí thứ sáu", function() {
+      expect(owl.order()).toBe(5);
+      expect(isInLine(owl, y_land)).toEqual(true);
     });
   });
   describe("Di chuyển nhân vật đến vị trí yêu cầu", function(){
-    it("Di chuyển con cú đến cạnh con gấu", function(){
-      moveToOrder(owl, bear.order() + 1, y_land);
+    it("Di chuyển con gấu đến cạnh con cú", function(){
+      moveToBeside(bear,owl,orderArray);
       expect(isBeside(owl,bear)).toEqual(true);
     });
     it("Di chuyển con gấu mèo đến cạnh con sói", function(){
-      moveToOrder(raccoon, wolf.order() + 1, y_land);
-      expect(isBeside(raccoon,wolf)).toEqual(true);
+      moveToBeside(raccoon,wolf,orderArray);
+      expect(isBeside(raccoon,wolf)).toEqual(true);      
     });
     it("Di chuyển con cáo đến cạnh con gấu mèo", function(){
-      moveToOrder(raccoon, wolf.order() + 1, y_land);
-      moveToOrder(fox, raccoon.order() + 1, y_land);
-      expect(isBeside(fox,raccoon)).toEqual(true);
+      moveToBeside(fox,raccoon,orderArray);
+      expect(isBeside(fox,raccoon)).toEqual(true);      
     });
+    
     it("Di chuyển con thỏ vào giữa con cáo và con gấu", function(){
-      moveToOrder(raccoon, wolf.order() + 1, y_land);
-      moveToOrder(fox, raccoon.order() + 1, y_land);
-      moveToOrder(rabbit, fox.order() + 1,y_land);
+      moveToBetween(rabbit,fox,bear,orderArray);
       expect(isBetween(fox, bear, rabbit)).toEqual(true);
     });
   });
 });
+
--- Code/Unitest/src/PlaceAnimals.js
+++ Code/Unitest/src/PlaceAnimals.js
@@ -5,12 +5,12 @@ class Character {
     }
     order() {
 
-        if(this.x >= 0 && this.x + 140 <= 150) return 1;
-        if(this.x >= 150 && this.x + 140 <= 300) return 2;
-        if(this.x >= 300 && this.x + 140 <= 450) return 3;
-        if(this.x >= 450 && this.x + 140 <= 600) return 4;
-        if(this.x >= 600 && this.x + 140 <= 750) return 5;
-        if(this.x >= 750 && this.x + 140 <= 900) return 6;
+        if(this.x >= 0 && this.x + 140 <= 150) return 0;
+        if(this.x >= 150 && this.x + 140 <= 300) return 1;
+        if(this.x >= 300 && this.x + 140 <= 450) return 2;
+        if(this.x >= 450 && this.x + 140 <= 600) return 3;
+        if(this.x >= 600 && this.x + 140 <= 750) return 4;
+        if(this.x >= 750 && this.x + 140 <= 900) return 5;
         else return -1;
     }
 }
@@ -18,34 +18,55 @@ function isInLine(Character, y_land) {
     if(Character.y == y_land) return true;
     else return false;
 }
-function moveToOrder(Character,order,y_land){
-    Character.y = y_land;
-    switch(order){
-        case 1: {
-            Character.x = 5;
-            break;
+function moveToBeside(MoveCharacter, InLineCharacter, orderArray){
+    var index = InLineCharacter.order();
+    if(index >= 0) {
+        if(index == 0) {
+            if(orderArray[1] == 1) return;
+            else {
+                MoveCharacter.x = 155;
+                MoveCharacter.y = InLineCharacter.y;
+                orderArray[1] = 1; 
+            }
         }
-        case 2: {
-            Character.x = 155;
-            break;
+        else if(index == 5) {
+            if(orderArray[4] == 1) return;
+            else {
+                MoveCharacter.x = 605;
+                MoveCharacter.y = InLineCharacter.y;
+                orderArray[4] = 1;
+            }
         }
-        case 3: {
-            Character.x = 305;
-            break;
+        else {
+            if(orderArray[index + 1] == 0) {
+                MoveCharacter.x = (index + 1) * 150 + 5;
+                MoveCharacter.y = InLineCharacter.y;
+                orderArray[index + 1] = 1;
+                return;
+            }
+            else if(orderArray[index - 1] == 0) {
+                MoveCharacter.x = (index - 1) * 150 + 5;
+                MoveCharacter.y = InLineCharacter.y;
+                orderArray[index - 1] = 1;
+                return;
+            }
+            else return;
         }
-        case 4: {
-            Character.x = 455;
-            break;
-        }
-        case 5: {
-            Character.x = 605;
-            break;
-        }
-        case 6: {
-            Character.x = 755;
-            break;
-        }
+        
     }
+    else return;
+    
+}
+function moveToBetween(MoveCharacter, InLineCharacter1, InLineCharacter2, orderArray) {
+    var index1 = InLineCharacter1.order() ;
+    var index2 = InLineCharacter2.order() ;
+    var moveIndex = (index1 + index2)/2;
+    if(orderArray[moveIndex] == 0){    
+        MoveCharacter.x = moveIndex * 150 + 5;
+        MoveCharacter.y = InLineCharacter1.y;
+        orderArray[moveIndex] = 1;
+    }
+    else return;
 }
 
 function isBeside(FirstCharecter, SecondCharacter){
